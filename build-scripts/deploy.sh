28e7bee547b701be65c0a230b146a36bbc6356a6
ATAROY <atanu.roy@barco.com>
2021-04-03 16:18:44 +0530

    Committing forced copy

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "Starting Deploy operations.............................."
 DEPLOYMENT_SERVER="139.59.25.64"
 
 echo "Moving inside Artifact directory and copying files..."
-sshpass -p ${slave_pass} ssh root@${DEPLOYMENT_SERVER} "ls -lash; cd artifacts; pwd; unzip $(ls -t *.zip | head -n1); cd basic-flexbox; pwd; cp * /var/www/g1n1/"
+sshpass -p ${slave_pass} ssh root@${DEPLOYMENT_SERVER} "ls -lash; cd artifacts; pwd; rm -rf basic-flexbox; unzip $(ls -t *.zip | head -n1); cd basic-flexbox; pwd; \cp * /var/www/html/g1n1/"
 
 echo "Restarting server..."
 sshpass -p ${slave_pass} ssh root@${DEPLOYMENT_SERVER} service nginx stop
