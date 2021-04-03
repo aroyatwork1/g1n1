66ba6451deae48ee1c0605963197f84bae71cded
ATAROY <atanu.roy@barco.com>
2021-04-03 13:15:49 +0530

    Commtting modified build script and new HTML folder which is intended to deployed as artifact on staging server

--- build-scripts/publish.sh
+++ build-scripts/publish.sh
@@ -1,9 +1,27 @@
 echo "Starting Publish operations.............................."
 
+WORkSPACE = "/root/gen_node_workspace/workspace/g1n1"
+VERSION = "$(date +%s)"
+ARTIFACT_NAME = "g1n1_archive.${VESRION}.zip"
+DEPLOYMENT_SERVER = "139.59.25.64"
+
 # Loop for 5 seconds and exit
 for i in {1..5}
     do
         sleep 1s
     done
+
+echo "Moving inside build workspace..."
+cd  $WORkSPACE
+pwd
+
+echo "Archiving the build artifact..."
+zip -r ${ARTIFACT_NAME} basic-flexbox/
+
+echo "Copying generated artifact to staging folder..."
+cp ${ARTIFACT_NAME} /root/artifacts/
+
+echo "Moving the staged artifact to deployment server ${DEPLOYMENT_SERVER}..."
+sshpass -p "96MZjebtMW4-g8C" scp -pC /root/artifacts/${ARTIFACT_NAME} root@${DEPLOYMENT_SERVER}:/root/artifacts/
 
 echo "Finished Publish operations.............................."
