41b1b255042572fe1c0b30f6f988fad040660617
ATAROY <atanu.roy@barco.com>
2021-04-24 22:57:48 +0530

    Committing minor patch to build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -18,5 +18,5 @@ docker push ${G1N1_IMAGE_NAME}
 rm /root/.docker/config.json
 docker logout
 
-echo G1N1_IMAGE_NAME > ~/g1n1_tmp
+echo ${G1N1_IMAGE_NAME} > ~/g1n1_tmp
 echo "Finished Build operations.............................."
