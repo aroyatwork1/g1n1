e7d89b2e3f946bb2e10f98923e266be21087353a
ATAROY <atanu.roy@barco.com>
2021-04-24 17:14:11 +0530

    Adding facility to auto-install make

--- build-scripts/start.sh
+++ build-scripts/start.sh
@@ -11,7 +11,23 @@ if [ -x "$(command -v docker)" ]; then
     echo "Docker is installed"
 else
     echo "*** DOCKER NOT FOUND ***"
-    exit 1
+    echo "Trying to install Docker..."
+    apt-get remove docker docker-engine docker.io containerd runc
+    rm -rf /var/lib/docker
+    apt-get update
+    apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io
+    echo "Installed Docker..."
 fi
 
+if [ -x "$(command -v make)" ]; then
+    echo "Docker is installed"
+else
+    echo "*** MAKE NOT FOUND ***"
+    echo "Trying to install make..."
+    apt-get -y install make
+fi
 echo "Pre-requisite  checks done................................"
